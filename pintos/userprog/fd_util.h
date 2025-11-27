48c7178ab891140466ea41aeea69dac400899897
Jeongns <jhlee030313@gmail.com>
2025-11-27 22:05:20 +0900

    init: base code

---
+++ pintos/userprog/fd_util.h
@@ -0,0 +1,21 @@
+#ifndef FD_UTIL_H
+#define FD_UTIL_H
+
+#include <stdbool.h>
+
+#include "filesys/file.h"
+#include "threads/thread.h"
+
+extern struct file* stdin_entry;
+extern struct file* stdout_entry;
+
+struct fd_table;
+void init_std_fds();
+bool fd_init(struct thread* t);
+int fd_allocate(struct fd_table* fd_t, struct file* f);
+struct file* get_file(struct fd_table* fd_t, int fd);
+void fd_close(struct fd_table* fd_t, int fd);
+void fd_clean(struct thread* t);
+bool copy_fd_table(struct fd_table* dst, struct fd_table* src);
+int fd_dup2(struct fd_table* fd_t, int oldfd, int newfd);
+#endif
