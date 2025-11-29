7dd803b057241a4690590c3b93cfe4207d7e69a1
Thor0111 <buddhaandai@gmail.com>
2025-11-29 20:40:27 +0900

    spt구조체와 CRUD 구현

--- pintos/include/vm/vm.h
+++ pintos/include/vm/vm.h
@@ -2,6 +2,7 @@
 #define VM_VM_H
 #include <stdbool.h>
 #include "threads/palloc.h"
+#include "hash.h"
 
 enum vm_type {
 	/* page not initialized */
@@ -34,7 +35,7 @@ enum vm_type {
 struct page_operations;
 struct thread;
 
-#define VM_TYPE(type) ((type)&7)
+#define VM_TYPE(type) ((type) & 7)
 
 /* The representation of "page".
  * This is kind of "parent class", which has four "child class"es, which are
@@ -46,6 +47,7 @@ struct page {
 	struct frame *frame; /* Back reference for frame */
 
 	/* Your implementation */
+	struct hash_elem spt_hash_elem;
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
@@ -86,6 +88,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash spt_hash;
 };
 
 #include "threads/thread.h"
